d79503f03ed00d71832afdea5d974e934dfa48fc
Jay Ramsden <22206026@bucks.ac.uk>
2022-10-07 11:13:11 +0100

    did the 7 times table, started grade conversion, 'else' without 'if' needs looking into. try out week 1 tasks and need notes doing

--- src/Week2/Week02.java
+++ src/Week2/Week02.java
@@ -1,7 +1,10 @@
 package Week2;
 
-public class Week02 
+import helpers.InputReader;
+
+public class Week02
 {
+    public static final int SEVEN = 7;
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
     public static void main(String[] args)
     {
@@ -15,5 +18,45 @@ public class Week02
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
-    }     
+
+        do7TimesTable();
+        //doConvertGrade();
+    }
+
+    public static void do7TimesTable()
+    {
+        int value; int product;
+
+        for(value = 1; value <= 12; value++)
+        {
+            product = value * SEVEN;
+            System.out.println("The product of "+ value + " multiplied by 7 equals " + product);
+        }
+    }
+
+    public static void doConvertGrade()
+    {
+        String value = InputReader.getString("Please enter the grade you received on your last assignment -> ");
+
+        char letter ; letter = A;
+
+        letter = value.charAt(0); letter = A; value = "A";
+
+        if(value.equals("A")) value = "A";
+        {
+            System.out.println("Your grade is First Class");
+        }
+        else if(value.equals("B")) value = "A";
+        {
+            System.out.println("Your grade is Upper Second Class");
+        }
+
+        else
+        {
+            System.out.println("Loser");
+        }
+
+        letter = value.charAt(0); letter = A; value = "A";
+
+    }
 }
